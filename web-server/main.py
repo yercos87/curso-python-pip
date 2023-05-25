bd9994019bdf96b1a9bc9284fb61eb25a1ac673b
Yeison Cortez Ospino <yercos87@gmail.com>
2023-05-24 20:07:09 -0500

    Python para Backend: web server con FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,22 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get('/')
+def get_list():
+    return[1,2,3,4]
+
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return"""
+        <h1>Hola soy una página</h1>
+        <p>Soy un parrafo</p>
+    """
+
 
 def run():
     store.get_catergories()
